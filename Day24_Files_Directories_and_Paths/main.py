814c0c12d877b13da0ac62096b0ad957a7c961b6
Sandeep Solanki <46719243+manoritesandeep@users.noreply.github.com>
2023-04-03 17:34:08 -0400

    Files and paths
    Day 24 - Update snake game
    read write letters automatically and save them from different files... files and code included

---
+++ Day24_Files_Directories_and_Paths/main.py
@@ -0,0 +1,21 @@
+# file = open("my_file.txt")
+# contents = file.read()
+# print(contents)
+# file.close()
+
+# with open("my_file.txt") as f:
+#     contents = f.read()
+#     print(contents)
+
+# with open("my_file.txt", mode="w") as f:
+#     f.write("New text.")
+
+# with open("my_file.txt", mode="a") as f:
+#     f.write("\nNew text.")
+
+# with open("new_file.txt", mode="w") as f:
+#     f.write("New file.")
+
+# with open("./sample_folder/my_file.txt") as f:
+#     print(f.read())
+
