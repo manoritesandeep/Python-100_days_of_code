064c0627024d4fe4095310c7ae7bda5522c1212c
Sandeep Solanki <46719243+manoritesandeep@users.noreply.github.com>
2023-04-20 07:51:40 -0500

    Day 28 codes
    Dynamic Typing, Pomodoro app (Timer - Work_Rest_work_rest)... Enjoy!!

---
+++ Day28_Tkinter_DynamicTyping_PomodoroGUIapp/rough_work.py
@@ -0,0 +1,15 @@
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+# import time
+#
+# count = 5
+# while True:
+#     time.sleep(1)
+#     count -= 1
+#     print(count)
+
+
+print(100 % 60)
+
+import math
+print(100/60)
+print(math.floor(100/60))
