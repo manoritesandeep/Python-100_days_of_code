ca61ca1cabd7ff744cbc6f1b97d5027b0b24a3a6
Sandeep Solanki <46719243+manoritesandeep@users.noreply.github.com>
2023-04-24 21:20:15 -0500

    codes and note day 35
    codes and note day 35

---
+++ Day35_Keys_Authentication_EnvironmentVariables_SendSMS/rough_work.py
@@ -0,0 +1,29 @@
+# import requests
+# from twilio.rest import Client
+# import os
+
+#
+# OMW_endpoint_hr = "https://pro.openweathermap.org/data/2.5/forecast/hourly"
+# weather_params = {
+#     "LAT": 40.477090,
+#     "LONG": -88.993220,
+#     "exclude": "current,minutely,daily",
+#     "appid": API_KEY1,
+# }
+#
+# OMW_endpoint = "https://api.openweathermap.org/data/3.0/onecall"
+# # OMW_endpoint_hr = "https://pro.openweathermap.org/data/2.5/forecast/hourly"
+#
+# # response = requests.get(OMW_endpoint, params=weather_params)
+# # print(response.json())
+#
+
+# print(weather_data["hourly"][0]["weather"][0]["id"])
+
+
+
+
+
+
+
+
